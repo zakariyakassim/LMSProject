ecbd24109235e9cec3cd5e6f9f8dad44e81b2eee
QAJamesClive <jordan.harrison@academytrainee.com>
2018-09-12 10:20:22 +0100

    finished create tavke to hold course details

--- src/main/java/com/qa/lmsproject/repository/CourseRepository.java
+++ src/main/java/com/qa/lmsproject/repository/CourseRepository.java
@@ -1,5 +1,9 @@
 package com.qa.lmsproject.repository;
 
-public class CourseRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.qa.lmsproject.model.CourseModel;
+
+public interface CourseRepository extends JpaRepository<CourseModel,Long>{
 
 }
---
+++ src/test/java/intergration/CourseIntergrationTest.java
@@ -0,0 +1,94 @@
+package intergration;
+
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.qa.lmsproject.LmsprojectApplication;
+import com.qa.lmsproject.model.CourseModel;
+import com.qa.lmsproject.repository.CourseRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {LmsprojectApplication.class})
+@AutoConfigureMockMvc
+public class CourseIntergrationTest {
+
+	@Autowired
+	private MockMvc mvc;
+	
+	@Autowired
+	private CourseRepository repo;
+	
+	@Before
+	public void clearDB() {
+		repo.deleteAll();
+	}
+	
+	@Test
+	public void findingAndRetrivingFromDatabase()
+		throws Exception{
+		repo.save(new CourseModel("Ifty's farm yard animals","Come along and join Ifty for some family fun!"));
+		mvc.perform(get("/api/course")
+		.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(status() 
+		.isOk())
+		.andExpect(content()
+		.contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+		.andExpect(jsonPath("$[0].name", is("Ifty's farm yard animals")));
+	}
+	
+	@Test
+	public void createCourseTest()
+			throws Exception{
+			mvc.perform(MockMvcRequestBuilders.post("/api/course")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content("{\"name\" : \"Ifty's farm yard animals\", \"description\" : \"Come along and join Ifty for some family fun!\"}"))
+			.andExpect(status()
+			.isOk())
+			.andExpect(content()
+			.contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+			.andExpect(jsonPath("$.name", is("Ifty's farm yard animals")));
+	}
+	
+	@Test
+	public void deleteCourseTest()
+			throws Exception{
+			CourseModel course = new CourseModel("Ifty's farm yard animals","Come along and join Ifty for some family fun!");
+			repo.save(course);
+			System.out.println("ID: " + course.getId());
+			mvc.perform(MockMvcRequestBuilders.delete("/api/course/" + course.getId())
+			.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk());
+			System.out.println("ID: " + course.getId());
+	}
+	
+	@Test
+	public void updateCourseTest() throws Exception {
+		CourseModel course = new CourseModel("Ifty's farm yard animals","Come along and join Ifty for some family fun!");
+		repo.save(course);
+		mvc.perform(MockMvcRequestBuilders.put("/api/course/" + course.getId())
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{\"name\" : \"James farm yard animals\", \"description\" : \"Come along and join James for some family fun!\"}"))
+				.andExpect(status()
+				.isOk())
+				.andExpect(content()
+				.contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.name", is("James farm yard animals")));
+	}
+
+}
