e8332d0606617bfe58e20cd743c51301bc652b31
KrisDuff <Kristopher.Duff@academytrainee.com>
2018-09-17 10:42:40 +0100

    Navigation fix
    Fixed navigation

--- frontend/src/main/Login.jsx
+++ frontend/src/main/Login.jsx
@@ -8,6 +8,11 @@ import Col from "react-bootstrap/es/Col";
 import {Checkbox} from "react-bootstrap";
 import FormControl from "react-bootstrap/es/FormControl";
 import ControlLabel from "react-bootstrap/es/ControlLabel";
+import {Nav} from 'react-bootstrap';
+import AuthService from './AuthService';
+
+import axios from 'axios';
+import './Login.css';
 
 class Login extends React.Component {
     constructor(props, context) {
@@ -16,35 +21,80 @@ class Login extends React.Component {
         this.state = {
             show: false
         };
+        this.user = {
+            userName: '',
+            password: ''
+        };
+
+        this.Auth = new AuthService();
     }
+
     handleHide() {
-        this.setState({ show: false });
+        this.setState({show: false});
     }
+
+    handleShow() {
+        this.setState({show: true});
+    }
+
+    onChange = (e) => {
+        this.user({[e.target.name]: e.target.value});
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log("Account has logged in ");
+
+        this.Auth.login('jharry', 'test')
+            .then(res => {
+                this.props.history.replace('/');
+                console.log(res.data)
+            })
+            .catch(err => {
+                alert(err);
+            });
+
+        axios('http://192.168.1.111:8080/api/user', {
+            method: 'POST',
+            auth: {
+                username: 'jharry',
+                password: 'test'
+            }
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    };
+
     render() {
         return (
-          <div className="modal-container">
-             
-			 <NavItem onClick={() => this.setState({ show: true })}>LOGIN</NavItem>
-            
+            <div className="modal-container">
+
+                <p onClick={() => this.setState({show: true})} href="#">LOGIN</p>
+
                 <Modal
                     show={this.state.show}
                     onHide={this.handleHide}
                     container={this}
                     aria-labelledby="contained-modal-title">
-					
+
                     <Modal.Header closeButton>
                         <Modal.Title id="contained-modal-title">
-						 Login
+                            Login
                         </Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
-                        <Form horizontal>
-                            <FormGroup controlId="formHorizontalEmail">
+                        <Form className="form" horizontal>
+                            <FormGroup  controlId="formHorizontalEmail">
                                 <Col componentClass={ControlLabel} sm={2}>
                                     Username
                                 </Col>
                                 <Col sm={10}>
-                                    <FormControl type="input" placeholder="Username" />
+                                    <FormControl className="loginBar" type="input" placeholder="Username"/>
                                 </Col>
                             </FormGroup>
 
@@ -53,19 +103,13 @@ class Login extends React.Component {
                                     Password
                                 </Col>
                                 <Col sm={10}>
-                                    <FormControl type="password" placeholder="Password" />
+                                    <FormControl className="loginBar" type="password" placeholder="Password"/>
                                 </Col>
                             </FormGroup>
 
                             <FormGroup>
                                 <Col smOffset={2} sm={10}>
-                                    <Checkbox>Remember me</Checkbox>
-                                </Col>
-                            </FormGroup>
-
-                            <FormGroup>
-                                <Col smOffset={2} sm={10}>
-                                    <Button type="submit">Sign in</Button>
+                                    <Button type="submit" onClick={this.handleSubmit}>Sign in</Button>
                                 </Col>
                             </FormGroup>
                         </Form>
--- frontend/src/main/MainNav.jsx
+++ frontend/src/main/MainNav.jsx
@@ -9,24 +9,27 @@ import './MainNav.css';
 const MainNav =() => {
   return(
   <Navbar id="navigation">
-  <Navbar.Brand>
-  <a href="/">QC Tutorials</a>
-  </Navbar.Brand>
+   <Navbar.Brand >
+   <Nav pullLeft className="site-name" >
+     <a href="/"> QC Tutorials</a>
+  </Nav>
+   </Navbar.Brand>
    <Nav pullRight>
-    <NavItem>
-        <FormControl type='text' placeholder='find course here'/> 
-    </NavItem>
-    <NavItem> <Button type='submit'>Search</Button></NavItem>
+	  <NavItem><Login/></NavItem>
+    </Nav>
+   <Nav pullRight>
     <NavItem eventKey={1} href="/">HOME</NavItem>
 	<NavItem  eventKey={2} href="/courses">COURSES</NavItem>
 	<NavDropdown  eventKey="3" title="ACCOUNT" id="nav-dropdown">
           <MenuItem eventKey="3.1" href="/trainerProfile">My Profile</MenuItem>
           <MenuItem eventKey="3.3" href="/leaners">My Learners</MenuItem>
 		  <MenuItem href="/AddCourse">Add Course</MenuItem>
-        </NavDropdown>
-	<NavItem eventKey={4} href="/learnerProfile">PROFILE</NavItem>
-	<NavItem eventKey={5} > <Login/> </NavItem>
-   </Nav>
+    </NavDropdown>
+    </Nav>
+	<Nav className="Search">
+	 <NavItem ><FormControl className="search-bar"type='text' id="searchBar" placeholder='find course here'/></NavItem>
+     <NavItem ><Button type='submit'>Search</Button></NavItem >
+	</Nav>
   </Navbar>
 	  );
  }
